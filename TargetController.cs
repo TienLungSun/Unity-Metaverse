62c7a61d353112a1681af76cc30133ece1ab7e69
Tien-Lung Sun <39815127+TienLungSun@users.noreply.github.com>
2022-03-11 15:51:14 +0800

    Add files via upload

--- TargetController.cs
+++ TargetController.cs
@@ -87,8 +87,9 @@ namespace Unity.MLAgentsExamples
                 //onCollisionEnterEvent.Invoke(col);
                 //Debug.Log("Target hit by " + col.gameObject.name);
 
-                //search for the top parent
+                //search for top parent which is the RagDoll
                 GameObject walker = col.gameObject.transform.root.gameObject;
+                // call the TouchedTarget function to add rewards
                 walker.GetComponent<WalkerAgent>().TouchedTarget();
 
                 if (respawnIfTouched)
