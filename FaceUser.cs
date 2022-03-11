a248a96c43e9a7e6a9ee1618b0f003c61ab829f8
Tien-Lung Sun <39815127+TienLungSun@users.noreply.github.com>
2022-03-11 12:58:07 +0800

    Add files via upload

---
+++ FaceUser.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceUser : MonoBehaviour
+{
+    public Transform mainCamera;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(mainCamera);
+    }
+}
