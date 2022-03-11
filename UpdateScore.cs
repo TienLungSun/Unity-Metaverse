6d682e2fb97e69c1a4d1b0415d131511fa20d7e3
Tien-Lung Sun <39815127+TienLungSun@users.noreply.github.com>
2022-03-11 13:05:06 +0800

    Add files via upload

---
+++ UpdateScore.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    public GameObject walker;
+
+    void Start()
+    {
+        GetComponent<TextMesh>().text = "0";
+    }
+
+    void Update()
+    {
+        float r= walker.GetComponent < WalkerAgent>().GetCumulativeReward();
+        GetComponent<TextMesh>().text = r.ToString();
+    }
+}
